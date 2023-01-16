17c8df57a7de9ff541692b86fe819b0a2a13c67d
avachoi <72422987+avachoi@users.noreply.github.com>
2023-01-16 18:20:17 -0500

    Time: 572 ms (5.03%), Space: 51.6 MB (5.31%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+    let res=[];
+    let i=0;
+    let curr=1
+    while(i<Infinity){
+        if(!arr.includes(curr)){
+            res.push(curr);
+        }
+        curr++;
+        i++;
+        if(res.length===k){
+            return res[res.length-1]
+        }
+        console.log(res, curr)
+    }
+    return 'not found'
+};
+
+
+//[2,3,4,7,11]
+// i
+//curr=1
+//for loop 1000times
+//   push to res missting integer,
+//.  when missing.length===k, return missing's last integer
