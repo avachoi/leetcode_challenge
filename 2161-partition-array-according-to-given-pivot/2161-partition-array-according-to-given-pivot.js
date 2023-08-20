fd443500c74594995a4961d2f7676d93a690f13b
avachoi <72422987+avachoi@users.noreply.github.com>
2023-08-20 03:26:08 -0400

    Time: 250 ms (85.92%), Space: 95.3 MB (23.47%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
+var pivotArray = function(nums, pivot) {
+    let less=[];
+    let greater=[];
+    let middle=[]
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]<pivot){
+            less.push(nums[i])
+        }else if(nums[i]>pivot){
+            greater.push(nums[i])
+        }else{
+            middle.push(nums[i])
+        }
+    }
+    return [...less, ...middle, ...greater]
+};
