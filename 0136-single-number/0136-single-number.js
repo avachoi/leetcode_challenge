721f8f60eb553061f859116847f82a822cd9be0f
avachoi <72422987+avachoi@users.noreply.github.com>
2024-08-29 11:59:16 -0400

    Time: 56 ms (73.84%), Space: 49.6 MB (97.16%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    nums.sort((a,b)=>a-b);
-    for(let i=0; i<nums.length-1; i+=2){
-        if(nums[i]!==nums[i+1]){
-            return nums[i]
-        }
+    let result=0;
+    for(let i=0; i<nums.length; i++){
+        result ^=nums[i]
+        
+        
     }
-    return nums[nums.length-1]
+    return result
 };
-[1,1,2,2,4]
