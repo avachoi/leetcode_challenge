15b4d9bd1acf7b6a85af58c8b2e69cfbcd17d836
avachoi <72422987+avachoi@users.noreply.github.com>
2024-09-09 22:28:38 -0400

    Time: 1109 ms (5.44%), Space: 61.8 MB (5.08%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,5 +3,24 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    s.reverse()
+    let j=s.length-1
+    for(let i=0; i<s.length/2;i++){
+        [s[i],s[j]]=[s[j],s[i]];
+        j--
+        console.log(s)
+    }
 };
+
+
+
+// var reverseString = function (s) {
+//     let n = s.length;
+//     let i = 0, j = n - 1;
+//     while (i < j) {
+//         [s[i], s[j]] = [s[j], s[i]];
+//         i++;
+//         j--;
+//     }
+// };
+// We use destructuring assignment to swap the characters at indices i and j.
+// This is a concise way to swap two values in JS.
